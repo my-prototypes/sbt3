37be991f279871653f0d32a1417b503bc338e766
Armando Soares Sousa <armando.sousa@gmail.com>
2023-05-27 02:33:51 +0000

    correcao do ArquivoService

---
+++ src/main/java/br/es/ufpi/service/ArquivoService.java
@@ -0,0 +1,43 @@
+package br.es.ufpi.service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.es.ufpi.entity.Arquivo;
+import br.es.ufpi.repository.ArquivoRepository;
+
+@Service
+public class ArquivoService {
+    @Autowired
+    private ArquivoRepository arquivoRepository;
+
+    @Value("${app.upload-dir}")
+    private String uploadDir;
+
+    public void salvarArquivo(MultipartFile arquivo) {
+        try {
+            Path filePath = Path.of(uploadDir, arquivo.getOriginalFilename());
+            Files.copy(arquivo.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+            String caminho = filePath.toAbsolutePath().toString();
+            String nome = arquivo.getOriginalFilename();
+            Arquivo arquivoEntity = new Arquivo();
+            arquivoEntity.setNome(nome);
+            arquivoEntity.setCaminho(caminho);
+            arquivoRepository.save(arquivoEntity);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Arquivo> listarArquivos() {
+        return arquivoRepository.findAll();
+    }
+}
